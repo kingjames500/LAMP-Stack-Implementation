4e9c44ffd7c4a20770ce040219afe2725634e4eb
kingjames500 <jimmymaina500@gmail.com>
2025-05-26 10:50:58 +0300

    database credentials change to aliig with azure

--- classes/database/connection-class.php
+++ classes/database/connection-class.php
@@ -6,11 +6,11 @@ class databaseConnection
     protected function connection(){
         try {
             // Set database login details
-            $username = 'root';
-            $password = '1234';
+            $username = 'tkytvubhla';
+            $password = 'oSENi$ZDRs3b3iR6';
 
             // Create a new PDO connection to the 'portal' database
-            $dbConn = new PDO('mysql:host=localhost;dbname=portal', $username, $password);
+            $dbConn = new PDO('mysql:host=lamp-server;dbname=portal', $username, $password);
 
             // Set PDO to throw exceptions if an error happens
             $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
--- database.sql
+++ database.sql
@@ -1,26 +1,52 @@
+CREATE DATABASE IF NOT EXISTS `portal`;
+
+USE portal;
 
 CREATE TABLE IF NOT EXISTS `judges`(
     id varchar(36) PRIMARY KEY DEFAULT(UUID()),
     username varchar(20) not null,
     display_name varchar(25) not null
-    );
+);
 
-CREATE TABLE  IF NOT EXISTS `users`(
+INSERT INTO
+    judges(username, display_name)
+VALUES
+("Judge Simon", "Simon");
+
+INSERT INTO
+    judges(username, display_name)
+VALUES
+("Rose Waiyaki", "Rose W");
+
+INSERT INTO
+    judges(username, display_name)
+VALUES
+("Smith Lee", "Smith");
+
+CREATE TABLE IF NOT EXISTS `users`(
     id varchar(36) PRIMARY KEY DEFAULT(UUID()),
     full_name VARCHAR(100) NOT NULL
 );
 
-INSERT INTO users(full_name) VALUE("TEST1");
-INSERT INTO users(full_name) VALUE("test2");
-INSERT INTO users(full_name) VALUE("John Does");
+INSERT INTO
+    users(full_name) VALUE("TEST1");
+
+INSERT INTO
+    users(full_name) VALUE("test2");
+
+INSERT INTO
+    users(full_name) VALUE("John Does");
 
 CREATE TABLE IF NOT EXISTS `scores`(
-                        id varchar(36)  PRIMARY KEY DEFAULT(UUID()),
-                        judge_id varchar(36),
-                        user_id varchar(36),
-                        points INT CHECK (points >= 0 AND points <= 100),
-                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                        FOREIGN KEY (judge_id) REFERENCES judges(id),
-                        FOREIGN KEY (user_id) REFERENCES users(id),
-                        UNIQUE (judge_id, user_id)
+    id varchar(36) PRIMARY KEY DEFAULT(UUID()),
+    judge_id varchar(36),
+    user_id varchar(36),
+    points INT CHECK (
+        points >= 0
+        AND points <= 100
+    ),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (judge_id) REFERENCES judges(id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    UNIQUE (judge_id, user_id)
 );
