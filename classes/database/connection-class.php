7da0d4bea5a2accfa789d8e2128378e53a0775d7
kingjames500 <jimmymaina500@gmail.com>
2025-05-26 15:14:23 +0300

    adding ssl cert on my connection

--- classes/database/connection-class.php
+++ classes/database/connection-class.php
@@ -8,9 +8,13 @@ class databaseConnection
             // Set database login details
             $username = 'tkytvubhla';
             $password = 'oSENi$ZDRs3b3iR6';
+            $options = [
+                PDO::MYSQL_ATTR_SSL_CA => __DIR__ . '../cert/DigiCertGlobalRootCA.crt.pem',
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => true,
+            ];
 
             // Create a new PDO connection to the 'portal' database
-            $dbConn = new PDO('mysql:host=lamp-server.mysql.database.azure.com;dbname=portal', $username, $password);
+            $dbConn = new PDO('mysql:host=lamp-server.mysql.database.azure.com;dbname=portal', $username, $password, $options);
 
             // Set PDO to throw exceptions if an error happens
             $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
